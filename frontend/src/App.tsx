0e4e89e498d8737383bd198513feafaa54bba1b3
StevenCPY <scpy21@gmail.com>
2026-08-25 23:11:48 -0400

    Integrated WebSocket agent calls into frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function NoScreenshotText() {
 
 function App() {
 	const [prompt, setPrompt] = useState("")
-	const [notification, setNotification] = useState("here")
+	const [notification, setNotification] = useState("")
 	const [screenshot, setScreenshot] = useState<string|null>(null)
 
 	const socket = useMemo(() => io("http://localhost:3000"), [])
@@ -34,9 +34,12 @@ function App() {
 					{notification}
 				</div>
 				<textarea value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Enter workflow..." rows={10} />
-				<div>
-					<button id="example-workflow-button" onClick={ () => setPrompt(workflow) }>Try example workflow</button>
-					<button id="clear-button" onClick={ () => setPrompt("") }>Clear</button>
+				<div id="buttons-container">
+					<div id="example-and-clear-buttons-container">
+						<button id="example-workflow-button" onClick={ () => setPrompt(workflow) }>Paste example workflow</button>
+						<button id="clear-button" onClick={ () => setPrompt("") }>Clear</button>
+					</div>
+					<button disabled={prompt === ""} id="submit-button" onClick={ () => {socket.emit("callAgent", prompt); setPrompt("") } }>Submit</button>
 				</div>
 			</div>
 		</div>
